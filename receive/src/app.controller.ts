65a2856371c6ec3b77daa56af082908f92b76de4
Mazen Elsayegh <melsayegh@ultatel.com>
2023-10-03 11:51:24 +0300

    finalize

--- receive/src/app.controller.ts
+++ receive/src/app.controller.ts
@@ -1,15 +1,6 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Post,
-  UseInterceptors,
-  Response,
-} from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
-import { FileInterceptor } from '@nestjs/platform-express';
 import * as fs from 'fs';
-import * as stream from 'stream';
 
 @Controller()
 export class AppController {
@@ -21,35 +12,30 @@ export class AppController {
   }
 
   @Post('receive')
-  @UseInterceptors(FileInterceptor('file'))
   async receiveFile(@Body() body: any) {
-    console.log(body.stream);
     try {
-      // Ensure the 'uploads' directory exists (create it if necessary)
-      const uploadDirectory = 'uploads'; // Replace with your desired directory
+      const uploadDirectory = 'uploads';
       if (!fs.existsSync(uploadDirectory)) {
         fs.mkdirSync(uploadDirectory);
       }
-
-      // Create a writable stream to save the file
-      const filePath = `${uploadDirectory}/test.txt`;
+      const filePath = `${uploadDirectory}/${body.filename}`;
       const writableStream = fs.createWriteStream(filePath);
-      // const dataBuffer = body.stream._readableState.buffer;
-      const dataBuffer = Buffer.from(
-        JSON.stringify(body.stream._readableState.buffer),
+      const dataBufferHead: Buffer = Buffer.from(
+        body.fss.stream._readableState.buffer.head.data.data,
       );
-      const readableStream = new stream.Readable();
-      readableStream.push(dataBuffer);
-      readableStream.push(null);
-      readableStream.pipe(writableStream);
-      // Listen for the 'finish' event, which indicates the file has been completely written
+
+      fs.writeFile(filePath, dataBufferHead, (err) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+
       writableStream.on('finish', () => {
         console.log(`File received and saved as: ${filePath}`);
       });
 
       return { message: 'File received and saved.' };
     } catch (error) {
-      // Handle errors here, e.g., log the error for debugging
       console.error(`Error saving the file: ${error.message}`);
       throw new Error(`Error saving the file: ${error.message}`);
     }
--- upload/src/app.controller.ts
+++ upload/src/app.controller.ts
@@ -9,10 +9,8 @@ import {
 import { AppService } from './app.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { Readable } from 'stream';
-import * as http from 'http';
-import { createReadStream } from 'fs';
-import { join } from 'path';
 import { HttpService } from '@nestjs/axios';
+import { lastValueFrom } from 'rxjs';
 
 @Controller()
 export class AppController {
@@ -33,10 +31,12 @@ export class AppController {
     fileStream.push(file.buffer);
     fileStream.push(null);
     const fss = new StreamableFile(fileStream);
-    const url = 'http://localhost:3002/receive'; // Replace with the URL of the other API
-
+    const url = 'http://localhost:3002/receive';
+    const filename = file.originalname;
     try {
-      const response = await this.httpService.post(url, fss).toPromise();
+      const response = await lastValueFrom(
+        this.httpService.post(url, { fss, filename }),
+      );
       return response.data;
     } catch (error) {
       // Handle errors here
@@ -45,49 +45,4 @@ export class AppController {
       );
     }
   }
-  // @Post('upload')
-  // @UseInterceptors(FileInterceptor('file'))
-  // uploadFile(@UploadedFile() file: Express.Multer.File) {
-  //   const fileStream = new Readable();
-  //   // const files = createReadStream(join(process.cwd(), 'package.json'));
-  //   fileStream.push(file.buffer);
-  //   fileStream.push(null);
-  //   const options = {
-  //     hostname: 'localhost',
-  //     port: 3002,
-  //     path: '/receive',
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/octet-stream', // Specify the content type as octet-stream
-  //     },
-  //   };
-  //   const request: any = http.request(options, (response) => {
-  //     // Handle the response from API 2
-  //     let responseData = '';
-
-  //     response.on('data', (chunk) => {
-  //       responseData += chunk;
-  //     });
-
-  //     response.on('end', () => {
-  //       console.log(responseData);
-  //     });
-  //   });
-
-  //   // Handle any request errors
-  //   request.on('error', (error) => {
-  //     console.error(error);
-  //   });
-  //   // console.log(request);
-
-  //   // Pipe the file stream into the request
-  //   const fss = new StreamableFile(fileStream);
-  //   request.body = fss;
-  //   fileStream.pipe(request);
-  //   // console.log(request);
-
-  //   // console.log('after');
-
-  //   return 'request';
-  // }
 }
